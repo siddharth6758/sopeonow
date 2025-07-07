2422a1486edf835b03ec8c1e3513143b039920f7
Siddharth Kundu <siddharth.kundu@drcsystems.com>
2025-07-07 15:09:48 +0530

    add: start project

---
+++ data/views.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from django.shortcuts import render
+from django.http import JsonResponse
+from .forms import CSVUploadForm
+
+def home(request):
+    if request.method == "POST":
+        csv_file = request.FILES.get("csv_file")
+        data = pd.read_csv(csv_file)
+        print('\n\nDATA:\n',data)
+        return JsonResponse({})
+    else:
+        csv_form = CSVUploadForm()
+        return render(request,'index.html', context={
+            "input_form": csv_form
+        })
